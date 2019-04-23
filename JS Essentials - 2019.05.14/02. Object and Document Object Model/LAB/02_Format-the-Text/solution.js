e8a860409ed318c7778b0029e5e857432b12d7b6
ErayErol <erayerolbmw@gmail.com>
2019-04-23 22:30:15 +0300

    Upload

---
+++ JS Essentials - 2019.05.14/02. Object and Document Object Model/LAB/02_Format-the-Text/solution.js
@@ -0,0 +1,13 @@
+function solve(){
+  let text = document.getElementById("input");
+
+    let textValue = text.value;
+    console.log(textValue);
+
+    if (textValue){
+    let splitedTextValue = textValue.split(".");
+
+
+
+  }
+}
