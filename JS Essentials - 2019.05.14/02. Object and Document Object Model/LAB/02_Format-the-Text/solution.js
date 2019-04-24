895a99848140070def1f35a0af469ccacb245bcb
ErayErol <erayerolbmw@gmail.com>
2019-04-24 16:24:39 +0300

    Upload

--- JS Essentials - 2019.05.14/02. Object and Document Object Model/LAB/02_Format-the-Text/solution.js
+++ JS Essentials - 2019.05.14/02. Object and Document Object Model/LAB/02_Format-the-Text/solution.js
@@ -1,13 +1,29 @@
-function solve(){
-  let text = document.getElementById("input");
+function solve() {
+    let textElement = document.getElementById('input');
+    let text = textElement.textContent;
+    let array = text.split('.').filter(function (x) { return x });
 
-    let textValue = text.value;
-    console.log(textValue);
+    let outputElement = document.getElementById('output');
+    let startIndex = 0;
+    let endIndex = 3;
 
-    if (textValue){
-    let splitedTextValue = textValue.split(".");
+    while (true) {
+        let result = '';
+        for (let i = startIndex; i < endIndex; i++) {
+            let sentence = array[i];
+            if (sentence !== undefined) {
+                result += `${sentence}. `;
+            }
+        }
 
+        let newElements = document.createElement('p');
+        newElements.textContent = result.trim();
+        outputElement.appendChild(newElements);
 
-
-  }
+        if (endIndex > array.length) {
+            break;
+        }
+        startIndex += 3;
+        endIndex += 3;
+    }
 }
