78cf3dbdd7e892079eb2d68a7bfd274af9880384
ErayErol <erayerolbmw@gmail.com>
2019-04-30 06:53:02 +0300

    Upload

--- JS Essentials - 2019.05.14/02. Object and Document Object Model/LAB/03_Growing-Word/solution.js
+++ JS Essentials - 2019.05.14/02. Object and Document Object Model/LAB/03_Growing-Word/solution.js
@@ -1,16 +1,17 @@
 function solve(){
-   let clicks = 0;
-   document.querySelector('button').addEventListener('click',
-   () => {
-     let p = document.querySelector('#exercise p');
-     if(clicks % 3 === 0){
-       p.style.color = "blue";
-     } else if (clicks % 3 === 1){
-       p.style.color = "green";
-     } else if(clicks % 3 === 2){
-       p.style.color = "red";
-     }
-     clicks++;
-     p.style.fontSize = `${clicks * 2}px`; });
- }
- 
+    let clicks = 0;
+
+    document.querySelector('button').addEventListener('click', function() {
+            let p = document.querySelector('#exercise p');
+
+            if(clicks % 3 === 0){
+                p.style.color = "blue";
+            } else if (clicks % 3 === 1){
+                p.style.color = "green";
+            } else if(clicks % 3 === 2){
+                p.style.color = "red";
+            }
+
+            clicks++;
+            p.style.fontSize = `${clicks * 2}px`; });
+}
