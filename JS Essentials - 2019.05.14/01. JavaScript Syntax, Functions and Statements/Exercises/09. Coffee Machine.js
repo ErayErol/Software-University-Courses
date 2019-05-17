4c41df301f6d7dd85430e6ae6cd9639ee484a5c5
ErayErol <erayerolbmw@gmail.com>
2019-05-17 10:35:15 +0300

    Upload

---
+++ JS Essentials - 2019.05.14/01. JavaScript Syntax, Functions and Statements/Exercises/09. Coffee Machine.js
@@ -0,0 +1,13 @@
+function solve(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let arrElement = arr[i].split(", ");
+
+        for (let j = 0; j < arrElement.length; j++) {
+            let currentInputElement = arrElement[j];
+            console.log(currentInputElement);
+        }
+            return;
+    }
+}
+
+solve(['1.00, coffee, caffeine, milk, 4', '0.40, tea, milk, 2', '1.00, coffee, decaf, 0']);
--- JS Essentials - 2019.05.14/01. JavaScript Syntax, Functions and Statements/Exercises/Training.js
+++ JS Essentials - 2019.05.14/01. JavaScript Syntax, Functions and Statements/Exercises/Training.js
@@ -1,10 +1,2 @@
-let num1 = Number(1);
-let num2 = Number(2);
 
-let string1 = "1";
-let string2 = "2";
-
-if (string2) {
-    console.log(num1 === num2);
-}
 
