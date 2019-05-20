b7edf12a9b0e538f881fc7878a7b4b77eafbcdb3
ErayErol <erayerolbmw@gmail.com>
2019-05-20 21:22:11 +0300

    Upload

--- JS Essentials - 2019.05.14/02. Object and Document Object Model/LAB/01. Towns to JSON.js
+++ JS Essentials - 2019.05.14/02. Object and Document Object Model/LAB/01. Towns to JSON.js
@@ -1,7 +1,28 @@
 function townsToJSON(inputArray) {
-    let tableHeadArray = inputArray.shift().split('|').filter(x => x !== "");
-    let townHead = tableHeadArray[0].trim();
+
+    let tableHeadArray = inputArray.shift().split("|").filter(x => x !== "");
+    let town = tableHeadArray[0].trim();
+    let latitude = tableHeadArray[1].trim();
+    let longitude = tableHeadArray[2].trim();
+
+    let obj = {};
+    let allTowns = [];
+    let allLat = [];
+    let allLon = [];
+
+    for (let i = 0; i < inputArray.length; i++) {
+        let currentRow = inputArray[i].split("|").filter(x => x !== "");
+
+        allTowns.push(`${currentRow[0].trim()}`);
+        allLat.push(`${currentRow[1].trim()}`);
+        allLon.push(`${currentRow[2].trim()}`);
+
+        obj[town] = allTowns;
+        obj[latitude] = allLat;
+        obj[longitude] = allLon;
+    }
     
+    console.log(`${JSON.stringify(obj)}`);
 }
 
 townsToJSON(['| Town | Latitude | Longitude |',
