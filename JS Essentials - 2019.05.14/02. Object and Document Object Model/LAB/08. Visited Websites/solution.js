af34b94e862c9ab2baff5f994a6136f773beeeb2
ErayErol <erayerolbmw@gmail.com>
2019-05-26 19:51:37 +0300

    Upload

--- JS Essentials - 2019.05.14/02. Object and Document Object Model/LAB/08. Visited Websites/solution.js
+++ JS Essentials - 2019.05.14/02. Object and Document Object Model/LAB/08. Visited Websites/solution.js
@@ -1,10 +1,20 @@
 function solve() {
-  let element = document.getElementsByClassName("link-1");
+  let siteElements = document.getElementsByClassName("link-1");
 
-  for (let i = 0; i < element.length; i++) {
-    let currentElement = element[i].getElementsByTagName("p")[0];
-    console.log(currentElement);
-    
-    
+  for (let siteElement of siteElements) {
+    siteElement.addEventListener("click", (e) => {
+      let currentTarget = e.currentTarget;
+
+      let paraElement = currentTarget.getElementsByTagName("p")[0];
+      let text = paraElement.textContent;
+      let textParts = text.split(" ");
+      let clicks = Number(textParts[1]);
+      clicks++;
+
+      textParts[1] = clicks;
+      paraElement.textContent = textParts.join(" ");
+    });
   }
+
+
 }
