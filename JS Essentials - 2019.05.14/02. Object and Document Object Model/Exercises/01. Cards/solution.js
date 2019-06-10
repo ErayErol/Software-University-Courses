974848daed90b95a25d4b12893d596707f92c59c
ErayErol <erayerolbmw@gmail.com>
2019-06-10 22:40:33 +0300

    Upload

--- JS Essentials - 2019.05.14/02. Object and Document Object Model/Exercises/01. Cards/solution.js
+++ JS Essentials - 2019.05.14/02. Object and Document Object Model/Exercises/01. Cards/solution.js
@@ -1,3 +1,43 @@
 function solve() {
-   // TODO...
+   let firstPlayerCardElements = document.querySelectorAll("#player1Div img");
+   let result = document.querySelectorAll("#result span");
+
+   let firstPlayerCardPower = 0;
+
+   for (let firstPlayerCardElement of firstPlayerCardElements) {
+      firstPlayerCardElement.addEventListener("click", function myFunc() {
+
+         firstPlayerCardPower = Number(firstPlayerCardElement.name);
+
+         let createCardNumber = document.createElement("span");
+         createCardNumber.textContent = firstPlayerCardPower;
+         
+         result[0].appendChild(createCardNumber);
+         firstPlayerCardElement.src = "images/whiteCard.jpg";
+         
+         firstPlayerCardElement.removeEventListener("click", myFunc, false);
+      });
+
+   }
+
+
+   let secondPlayerCardElements = document.querySelectorAll("#player2Div img");
+
+   let secondPlayerCardPower = 0;
+
+   for (let secondPlayerCardElement of secondPlayerCardElements) {
+      secondPlayerCardElement.addEventListener("click", function myFunc() {
+
+         secondPlayerCardPower = Number(secondPlayerCardElement.name);
+
+         let createCardNumber = document.createElement("span");
+         createCardNumber.textContent = secondPlayerCardPower;
+         
+         result[2].appendChild(createCardNumber);
+         secondPlayerCardElement.src = "images/whiteCard.jpg";
+         
+         secondPlayerCardElement.removeEventListener("click", myFunc, false);
+      });
+
+   }
 }
--- JS Essentials - 2019.05.14/02. Object and Document Object Model/LAB/08. Visited Websites/solution.js
+++ JS Essentials - 2019.05.14/02. Object and Document Object Model/LAB/08. Visited Websites/solution.js
@@ -1,11 +1,11 @@
 function solve() {
   let siteElements = document.getElementsByClassName("link-1");
+  console.log(siteElements);
+  
 
   for (let siteElement of siteElements) {
-    siteElement.addEventListener("click", function(e) {
-      let currentTarget = e.currentTarget;
-
-      let paragraphElement = currentTarget.getElementsByTagName("p")[0];
+    siteElement.addEventListener("click", function myFunc() {
+      let paragraphElement = siteElement.getElementsByTagName("p")[0];
       let text = paragraphElement.textContent;
       let textParts = text.split(" ");
       let clicks = Number(textParts[1]);
@@ -13,6 +13,8 @@ function solve() {
 
       textParts[1] = clicks;
       paragraphElement.textContent = textParts.join(" ");
+
+      siteElement.removeEventListener("click", myFunc, false);
     });
   }
 }
