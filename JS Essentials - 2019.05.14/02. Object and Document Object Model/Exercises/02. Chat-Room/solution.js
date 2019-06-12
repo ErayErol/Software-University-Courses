ca541ecbc0715d0dcefe6ee4c2fb67fbbf198b08
ErayErol <erayerolbmw@gmail.com>
2019-06-12 10:12:02 +0300

    Upload

--- JS Essentials - 2019.05.14/02. Object and Document Object Model/Exercises/02. Chat-Room/solution.js
+++ JS Essentials - 2019.05.14/02. Object and Document Object Model/Exercises/02. Chat-Room/solution.js
@@ -1,5 +1,38 @@
 function solve() {
-   //TODO...
+   let chatInput = document.getElementById('chat_input');
+   let chatMessages = document.getElementById('chat_messages');
+   
+   document.getElementById('send').addEventListener('click', clickEvent);
+   function clickEvent() {
+       let divElement = document.createElement('div');
+       divElement.textContent = chatInput.value;
+
+       if (divElement.textContent !== "") {
+          divElement.setAttribute('class', 'message my-message');
+          chatMessages.appendChild(divElement);
+          
+          chatInput.value = '';
+       }
+   }
 }
 
 
+// function solve() {
+//    let targetDiv = document.getElementsByClassName("my-message")[0];
+//    let textArea = document.getElementById("chat_input");
+//    let chatMessageArea = document.getElementById('chat_messages');
+   
+//    document.getElementById("send").addEventListener("click", clickEvent);
+   
+//    function clickEvent() {
+//       let targetDivClone = targetDiv.cloneNode(true);
+//       let textAreaContent = textArea.value;
+      
+//       if (textAreaContent !== "") {
+//          targetDivClone.textContent = textAreaContent;
+//          chatMessageArea.appendChild(targetDivClone);
+   
+//          textArea.value = "";
+//       }
+//    }
+// }
