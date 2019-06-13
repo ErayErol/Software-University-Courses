98c6f62d2e240f3775a4cdc58261de167c0f7a9f
ErayErol <erayerolbmw@gmail.com>
2019-06-13 18:39:28 +0300

    Upload

--- JS Essentials - 2019.05.14/02. Object and Document Object Model/Exercises/03. Number-Convertor/solution.js
+++ JS Essentials - 2019.05.14/02. Object and Document Object Model/Exercises/03. Number-Convertor/solution.js
@@ -1,4 +1,28 @@
 function solve() {
+    let selectMenuTo = document.getElementById("selectMenuTo");
 
-    //TODO...
+    let binaryOptionElement = document.createElement("option");
+    let hexadecimalOptionElement = document.createElement("option");
+
+    binaryOptionElement.text = "Binary";
+    binaryOptionElement.value = "binary";
+    hexadecimalOptionElement.text = "Hexadecimal";
+    hexadecimalOptionElement.value = "hexadecimal";
+
+    selectMenuTo.add(binaryOptionElement); // .add(binaryOptionElement, selectMenuTo[1])
+    selectMenuTo.add(hexadecimalOptionElement); // .add(hexadecimalOptionElement, selectMenuTo[2]
+
+    document.querySelector("button").addEventListener("click", clickEvent);
+    function clickEvent() {
+        let selectMenuToElementValue = document.getElementById('selectMenuTo').value;
+        let decimalElementValue = document.getElementById('input').value;
+        let resultElement = document.getElementById('result');
+
+        if (selectMenuToElementValue === 'binary') {
+            resultElement.value = Number(decimalElementValue).toString(2);
+        }
+        else if (selectMenuToElementValue === 'hexadecimal') {
+            resultElement.value = Number(decimalElementValue).toString(16).toUpperCase();
+        }
+    }
 }
--- JS Essentials - 2019.05.14/02. Object and Document Object Model/Exercises/04. JavaScript-Quizz/solution.js
+++ JS Essentials - 2019.05.14/02. Object and Document Object Model/Exercises/04. JavaScript-Quizz/solution.js
@@ -1,3 +1,49 @@
 function solve() {
-  //TODO...
+  let firstQuestionElement = document.querySelectorAll("h2")[0];
+  let secondQuestionElement = document.querySelectorAll("h2")[1];
+  let thirdQuestionElement = document.querySelectorAll("h2")[2];
+
+  let answerText1Right = document.getElementsByClassName("answer-text")[0].textContent;
+  let answerText2Wrong = document.getElementsByClassName("answer-text")[1].textContent;
+  let answerText3Wrong = document.getElementsByClassName("answer-text")[2].textContent;
+  let answerText4Right = document.getElementsByClassName("answer-text")[3].textContent;
+  let answerText5Right = document.getElementsByClassName("answer-text")[4].textContent;
+  let answerText6Wrong = document.getElementsByClassName("answer-text")[5].textContent;
+
+  Array.from(document.getElementsByTagName("p")).forEach((p) => {
+    p.addEventListener("click", clickEvent);
+  });
+
+  let point = 0;
+
+  function clickEvent(e) {
+    let currentTarget = e.currentTarget;
+
+    if (currentTarget.textContent === answerText1Right || currentTarget.textContent === answerText2Wrong) {
+      if (currentTarget.textContent === answerText1Right) {
+        point++;
+      }
+
+      firstQuestionElement.textContent = secondQuestionElement.textContent;
+      document.getElementsByTagName("p")[0].textContent = document.getElementsByTagName("p")[2].textContent;
+      document.getElementsByTagName("p")[1].textContent = document.getElementsByTagName("p")[3].textContent;
+    }else if (currentTarget.textContent === answerText3Wrong || currentTarget.textContent === answerText4Right) {
+      if (currentTarget.textContent === answerText4Right) {
+        point++;
+      }
+
+      firstQuestionElement.textContent = thirdQuestionElement.textContent;
+      document.getElementsByTagName("p")[0].textContent = document.getElementsByTagName("p")[4].textContent;
+      document.getElementsByTagName("p")[1].textContent = document.getElementsByTagName("p")[5].textContent;
+    }else if (currentTarget.textContent === answerText5Right || currentTarget.textContent === answerText6Wrong) {
+      if (currentTarget.textContent === answerText5Right) {
+        point++;
+        console.log(point);
+      }
+    }
+
+    if (point) {
+      
+    }
+  }
 }
