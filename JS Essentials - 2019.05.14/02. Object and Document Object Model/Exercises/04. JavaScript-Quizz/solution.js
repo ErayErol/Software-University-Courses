1339772e4c009f0c314e7f3c4633872f7f8f8e12
ErayErol <erayerolbmw@gmail.com>
2019-06-14 10:52:32 +0300

    Upload

--- JS Essentials - 2019.05.14/02. Object and Document Object Model/Exercises/04. JavaScript-Quizz/solution.js
+++ JS Essentials - 2019.05.14/02. Object and Document Object Model/Exercises/04. JavaScript-Quizz/solution.js
@@ -14,36 +14,59 @@ function solve() {
     p.addEventListener("click", clickEvent);
   });
 
-  let point = 0;
+  let rightAnswers = 0;
 
   function clickEvent(e) {
     let currentTarget = e.currentTarget;
 
     if (currentTarget.textContent === answerText1Right || currentTarget.textContent === answerText2Wrong) {
       if (currentTarget.textContent === answerText1Right) {
-        point++;
+        rightAnswers++;
       }
 
       firstQuestionElement.textContent = secondQuestionElement.textContent;
       document.getElementsByTagName("p")[0].textContent = document.getElementsByTagName("p")[2].textContent;
       document.getElementsByTagName("p")[1].textContent = document.getElementsByTagName("p")[3].textContent;
-    }else if (currentTarget.textContent === answerText3Wrong || currentTarget.textContent === answerText4Right) {
+    } else if (currentTarget.textContent === answerText3Wrong || currentTarget.textContent === answerText4Right) {
       if (currentTarget.textContent === answerText4Right) {
-        point++;
+        rightAnswers++;
       }
 
       firstQuestionElement.textContent = thirdQuestionElement.textContent;
       document.getElementsByTagName("p")[0].textContent = document.getElementsByTagName("p")[4].textContent;
       document.getElementsByTagName("p")[1].textContent = document.getElementsByTagName("p")[5].textContent;
-    }else if (currentTarget.textContent === answerText5Right || currentTarget.textContent === answerText6Wrong) {
+    } else if (currentTarget.textContent === answerText5Right || currentTarget.textContent === answerText6Wrong) {
       if (currentTarget.textContent === answerText5Right) {
-        point++;
-        console.log(point);
+        rightAnswers++;
       }
-    }
 
-    if (point) {
+
+      Array.from(document.getElementsByClassName("answer-text")).forEach((a) => {
+        a.style.display = "none";
+      });
+
+      Array.from(document.getElementsByClassName("answer-wrap")).forEach((a) => {
+        a.style.display = "none";
+      });
+    
+      Array.from(document.getElementsByClassName("question-wrap")).forEach((a) => {
+        a.style.display = "none";
+      });
+
+      let h1 = document.getElementsByTagName("h1");
+      let h1Clone = h1[0].cloneNode(true);
       
+      if (rightAnswers === 3) {
+        h1Clone.textContent = "You are recognized as top JavaScript fan!";
+      } else {
+        h1Clone.textContent = `You have ${rightAnswers} right answers`;
+      }
+
+      console.log(h1[0]);
+      
+      result.appendChild(h1Clone.textContent);
     }
   }
+
+  
 }
