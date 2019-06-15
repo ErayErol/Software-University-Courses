ca9eec970b028076939ed4be841bfef4de9fe5ba
ErayErol <erayerolbmw@gmail.com>
2019-06-15 15:16:36 +0300

    Upload

--- JS Essentials - 2019.05.14/02. Object and Document Object Model/Exercises/04. JavaScript-Quizz/solution.js
+++ JS Essentials - 2019.05.14/02. Object and Document Object Model/Exercises/04. JavaScript-Quizz/solution.js
@@ -1,72 +1,30 @@
 function solve() {
-  let firstQuestionElement = document.querySelectorAll("h2")[0];
-  let secondQuestionElement = document.querySelectorAll("h2")[1];
-  let thirdQuestionElement = document.querySelectorAll("h2")[2];
+  function clickEvent() {
+    if (this.textContent === correctAnswers[sectionNumber - 1]) {
+      playerPoints++;
+    }
 
-  let answerText1Right = document.getElementsByClassName("answer-text")[0].textContent;
-  let answerText2Wrong = document.getElementsByClassName("answer-text")[1].textContent;
-  let answerText3Wrong = document.getElementsByClassName("answer-text")[2].textContent;
-  let answerText4Right = document.getElementsByClassName("answer-text")[3].textContent;
-  let answerText5Right = document.getElementsByClassName("answer-text")[4].textContent;
-  let answerText6Wrong = document.getElementsByClassName("answer-text")[5].textContent;
+    if (sectionNumber === sectionElements.length - 2) {
+      let h1Element = document.getElementsByTagName('h1')[1];
+
+      if (playerPoints === sectionElements.length - 2) {
+        h1Element.textContent = 'You are recognized as top JavaScript fan!';
+      } else {
+        h1Element.textContent = `You have ${playerPoints} right answers`;
+      }
+    }
+
+    sectionElements[sectionNumber].style.display = 'none';
+    sectionElements[sectionNumber + 1].style.display = 'block';
+    sectionNumber++;
+  }
+
+  let sectionElements = document.getElementById('quizzie').children;
+  let sectionNumber = 1;
+  let playerPoints = 0;
+  let correctAnswers = ['onclick', 'JSON.stringify()', 'A programming API for HTML and XML documents'];
 
   Array.from(document.getElementsByTagName("p")).forEach((p) => {
     p.addEventListener("click", clickEvent);
   });
-
-  let rightAnswers = 0;
-
-  function clickEvent(e) {
-    let currentTarget = e.currentTarget;
-
-    if (currentTarget.textContent === answerText1Right || currentTarget.textContent === answerText2Wrong) {
-      if (currentTarget.textContent === answerText1Right) {
-        rightAnswers++;
-      }
-
-      firstQuestionElement.textContent = secondQuestionElement.textContent;
-      document.getElementsByTagName("p")[0].textContent = document.getElementsByTagName("p")[2].textContent;
-      document.getElementsByTagName("p")[1].textContent = document.getElementsByTagName("p")[3].textContent;
-    } else if (currentTarget.textContent === answerText3Wrong || currentTarget.textContent === answerText4Right) {
-      if (currentTarget.textContent === answerText4Right) {
-        rightAnswers++;
-      }
-
-      firstQuestionElement.textContent = thirdQuestionElement.textContent;
-      document.getElementsByTagName("p")[0].textContent = document.getElementsByTagName("p")[4].textContent;
-      document.getElementsByTagName("p")[1].textContent = document.getElementsByTagName("p")[5].textContent;
-    } else if (currentTarget.textContent === answerText5Right || currentTarget.textContent === answerText6Wrong) {
-      if (currentTarget.textContent === answerText5Right) {
-        rightAnswers++;
-      }
-
-
-      Array.from(document.getElementsByClassName("answer-text")).forEach((a) => {
-        a.style.display = "none";
-      });
-
-      Array.from(document.getElementsByClassName("answer-wrap")).forEach((a) => {
-        a.style.display = "none";
-      });
-    
-      Array.from(document.getElementsByClassName("question-wrap")).forEach((a) => {
-        a.style.display = "none";
-      });
-
-      let h1 = document.getElementsByTagName("h1");
-      let h1Clone = h1[0].cloneNode(true);
-      
-      if (rightAnswers === 3) {
-        h1Clone.textContent = "You are recognized as top JavaScript fan!";
-      } else {
-        h1Clone.textContent = `You have ${rightAnswers} right answers`;
-      }
-
-      console.log(h1[0]);
-      
-      result.appendChild(h1Clone.textContent);
-    }
-  }
-
-  
 }
--- JS Essentials - 2019.05.14/02. Object and Document Object Model/Exercises/05. Table-Search-Engine/solution.js
+++ JS Essentials - 2019.05.14/02. Object and Document Object Model/Exercises/05. Table-Search-Engine/solution.js
@@ -1,3 +1,24 @@
 function solve() {
-   //TODO...
+   let searchField = document.getElementById("searchField");
+   let rows = Array.from(document.querySelector("table tbody").children);
+   
+   let searchButtonElement = document.getElementById("searchBtn");
+   searchButtonElement.addEventListener("click", clickEvent);
+   
+   function clickEvent() {
+      let selectedRows = document.getElementsByClassName("select");
+      Array.from(selectedRows).forEach(row => row.classList.remove("select"));
+
+      for (let row of rows) {
+         let cells = Array.from(row.children);
+
+         for (let cell of cells) {
+            if (cell.textContent.toLowerCase().includes(searchField.value.toLowerCase())) {
+               row.classList.add("select");
+            }
+         }
+      }
+
+      searchField.value = "";
+   }
 }
