618332bddfe56cb1c8001593dd4816e65acf9995
ErayErol <erayerolbmw@gmail.com>
2019-06-21 19:00:26 +0300

    Upload

--- JS Essentials - 2019.05.14/02. Object and Document Object Model/Exercises/09. Order-the-Names/solution.js
+++ JS Essentials - 2019.05.14/02. Object and Document Object Model/Exercises/09. Order-the-Names/solution.js
@@ -1,2 +1,18 @@
 function solve() {
+    let buttonElement = document.getElementsByTagName('button')[0];
+    buttonElement.addEventListener('click', addToList);
+
+    function addToList() {
+        let input = document.getElementsByTagName('input')[0].value.toLowerCase();
+        let name = input[0].toUpperCase() + input.slice(1);
+
+        let charCode = name.charCodeAt(0);
+        let row = document.getElementsByTagName('li')[charCode - 65];
+
+        let names = row.textContent.split(', ').filter(n => n);
+        names.push(name);
+        row.textContent = names.join(', ');
+
+        document.getElementsByTagName('input')[0].value = '';
+    }
 }
