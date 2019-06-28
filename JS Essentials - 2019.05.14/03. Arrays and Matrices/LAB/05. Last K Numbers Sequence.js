b2b14415d7ef3b364066ca59db36ee11729aac3e
ErayErol <erayerolbmw@gmail.com>
2019-06-28 13:35:17 +0300

    Upload

--- JS Essentials - 2019.05.14/03. Arrays and Matrices/LAB/05. Last K Numbers Sequence.js
+++ JS Essentials - 2019.05.14/03. Arrays and Matrices/LAB/05. Last K Numbers Sequence.js
@@ -1,4 +1,25 @@
-function solve(){
+function solve(length, k) {
+    let sequence = [1];
+
+    while (length > 1) {
+        let total = 0;
+        let count = 0;
+        for (let i = sequence.length - 1; i >= 0; i--) {
+            if (count === k) {
+                break;
+            }
+            count++;
+
+            let currNum = sequence[i];
+            if (currNum !== undefined) {
+                total += Number(currNum);
+            }
+        }
+        sequence.push(total);
+        length--;
+    }
+
+    console.log(sequence.join(' '));
 }
 
 solve(6, 3);
