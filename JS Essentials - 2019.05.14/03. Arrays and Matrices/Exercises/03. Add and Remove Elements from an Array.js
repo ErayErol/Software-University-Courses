f6c779c9a5ae879374e08be8183f536f50f44251
ErayErol <erayerolbmw@gmail.com>
2019-08-05 15:55:19 +0300

    Upload

--- JS Essentials - 2019.05.14/03. Arrays and Matrices/Exercises/03. Add and Remove Elements from an Array.js
+++ JS Essentials - 2019.05.14/03. Arrays and Matrices/Exercises/03. Add and Remove Elements from an Array.js
@@ -1,28 +1,41 @@
 function sovle(arr) {
-    switch (arr.pop()) {
-        case value:
-            
-            break;
-    
-        default:
-            break;
+    let number = 1;
+
+    let result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        const command = arr[i];
+
+        switch (command) {
+            case "add":
+                result.push(number);
+                break;
+
+            case "add":
+                arr.pop();
+                break;
+        }
+
+        number++;
     }
+
+    console.log(result.join('\n'));
 }
 
-sovle(['add', 
-'add', 
-'add', 
-'add']
+sovle(['add',
+    'add',
+    'add',
+    'add']
 );
 
-sovle(['add', 
-'add', 
-'remove', 
-'add', 
-'add']
+sovle(['add',
+    'add',
+    'remove',
+    'add',
+    'add']
 );
 
-sovle(['remove', 
-'remove', 
-'remove']
+sovle(['remove',
+    'remove',
+    'remove']
 );
