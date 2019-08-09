c87d5ed4e6a5d5a81a30c40efe6adcdbbf2b2032
ErayErol <erayerolbmw@gmail.com>
2019-08-09 10:44:01 +0300

    Upload

--- JS Essentials - 2019.05.14/03. Arrays and Matrices/Exercises/04. Rotate Array.js
+++ JS Essentials - 2019.05.14/03. Arrays and Matrices/Exercises/04. Rotate Array.js
@@ -1,10 +1,25 @@
 function solve(arr) {
-    
+    let amountOfRotation = arr.pop();
+
+    for (let i = 0; i < amountOfRotation; i++) {
+        let element = arr.pop();
+        
+        arr.unshift(element);
+    }
+
+    console.log(arr.join(' '));
 }
 
-sovle(['1', 
-    '2', 
-    '3', 
-    '4', 
-    '2']
+solve(['1', 
+'2', 
+'3', 
+'4', 
+'2']
+);
+
+solve(['Banana', 
+'Orange', 
+'Coconut', 
+'Apple', 
+'15']
 );
