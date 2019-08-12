da6544398bb0b690ebe8a2ab98ce71da3057076f
ErayErol <erayerolbmw@gmail.com>
2019-08-12 16:46:57 +0300

    Upload

--- JS Essentials - 2019.05.14/03. Arrays and Matrices/Exercises/06. Sort an Array by 2 Criteria.js
+++ JS Essentials - 2019.05.14/03. Arrays and Matrices/Exercises/06. Sort an Array by 2 Criteria.js
@@ -1,4 +1,5 @@
 function solve(arr) {
+
     for (let i = 0; i < arr.length - 1; i++) {
         let element = arr[i];
         let element2 = arr[i + 1];
@@ -8,8 +9,33 @@ function solve(arr) {
             arr[i + 1] = element;
             i = -1;
         }
-        
     }
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        let element = arr[i];
+        let element2 = arr[i + 1];
+
+        if (element.length === element2.length) {
+
+            for (let j = 0; j < element.length; j++) {
+                let element3 = element[j].toLowerCase();
+                let element4 = element2[j].toLowerCase();
+
+                if (element3 > element4) {
+                    arr[i] = element2;
+                    arr[i + 1] = element;
+                    i = -1;
+                    break;
+                } else if (element3 < element4) {
+                    arr[i] = element;
+                    arr[i + 1] = element2;
+                    break;
+                }
+            }
+        }
+    }
+
+    console.log(arr.join('\n'));
 }
 
 solve(['alpha',
