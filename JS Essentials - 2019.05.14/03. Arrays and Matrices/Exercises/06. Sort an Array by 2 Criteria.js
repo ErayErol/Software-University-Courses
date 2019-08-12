b0c830e353ec42d916c22431363e2457b1f01928
ErayErol <erayerolbmw@gmail.com>
2019-08-12 10:53:35 +0300

    Upload

---
+++ JS Essentials - 2019.05.14/03. Arrays and Matrices/Exercises/06. Sort an Array by 2 Criteria.js
@@ -0,0 +1,21 @@
+function solve(arr) {
+    
+}
+
+solve(['alpha',
+    'beta',
+    'gamma']
+);
+
+solve(['Isacc',
+    'Theodor',
+    'Jack',
+    'Harrison',
+    'George']
+);
+
+solve(['test',
+    'Deny',
+    'omen',
+    'Default']
+);
