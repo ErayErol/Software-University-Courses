620aa7c16a553969f1e9e6644e28aa3b3b7b9f07
ErayErol <erayerolbmw@gmail.com>
2019-08-12 16:55:02 +0300

    Upload

--- JS Essentials - 2019.05.14/03. Arrays and Matrices/Exercises/02. Print every N-th Element from an Array.js
+++ JS Essentials - 2019.05.14/03. Arrays and Matrices/Exercises/02. Print every N-th Element from an Array.js
@@ -1,4 +1,31 @@
-function solve() {
+function solve(arr) {
+    let step = Number(arr.pop());
+
+    for (let i = 0; i < arr.length; i+=step) {
+        let element = arr[i];
+        console.log(element);
+    }
 }
 
-solve();
+solve(['5',
+    '20',
+    '31',
+    '4',
+    '20',
+    '2']
+);
+
+solve(['dsa',
+    'asd',
+    'test',
+    'tset',
+    '2']
+);
+
+solve(['1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6']
+);
