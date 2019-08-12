5eb123a4e7a614ffbb002b3a3ddb22e0924749a1
ErayErol <erayerolbmw@gmail.com>
2019-08-12 09:54:23 +0300

    Upload

--- JS Essentials - 2019.05.14/03. Arrays and Matrices/Exercises/06. Magic Matrices.js
+++ JS Essentials - 2019.05.14/03. Arrays and Matrices/Exercises/06. Magic Matrices.js
@@ -1,13 +1,32 @@
 function solve(matrix) {
+    let sum = 0;
+
     for (let row = 0; row < matrix.length; row++) {
+        let currSum = 0;
+
         for (let col = 0; col < matrix[row].length; col++) {
-            
-            console.log(matrix[row][col]);
+            currSum += matrix[row][col];
+        }
+
+        if (row === 0) {
+            sum = currSum;
+        }
+
+        if (sum !== currSum) {
+            console.log('false');
+            return;
         }
     }
+
+    console.log('true');
 }
 
 solve([[4, 5, 6],
 [6, 5, 4],
 [5, 5, 5]]
+);
+
+solve([[1, 2, 6],
+[6, 5, 4],
+[5, 2, 1]]
 );
