639c9c2575fa4b413b76eed98fc0c5772d3f4a70
ErayErol <erayerolbmw@gmail.com>
2019-08-18 16:50:19 +0300

    Upload

--- JS Essentials - 2019.05.14/04. String And RegEx/LAB/01_Pascal-or-Camel-Case/solution.js
+++ JS Essentials - 2019.05.14/04. String And RegEx/LAB/01_Pascal-or-Camel-Case/solution.js
@@ -1,3 +1,25 @@
 function solve() {
-  //TODO...
+  let input = document.getElementById("text").value;
+  let currentCase = document.getElementById("naming-convention").value;
+
+  function pascalOrCamelCase(input, currentCase) {
+    let words = input.toLowerCase().split(' ')
+      .filter(a => a !== '');
+
+    let output = "";
+    if (currentCase === "Pascal Case" || currentCase === "Camel Case") {
+      for (let word of words) {
+        output += word[0].toUpperCase() + word.substr(1).toLowerCase();
+      }
+
+      if (currentCase === "Camel Case") {
+        output = output[0].toLowerCase() + output.substr(1);
+      }
+    } else {
+      output = "Error!";
+    }
+    document.getElementById("result").innerHTML = output;
+  }
+
+  pascalOrCamelCase(input, currentCase);
 }
