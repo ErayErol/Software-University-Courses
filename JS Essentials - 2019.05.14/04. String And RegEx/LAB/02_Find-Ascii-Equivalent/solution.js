efccad37b03ab8ea601a346e125eee17f4792e4c
ErayErol <erayerolbmw@gmail.com>
2019-08-19 11:05:22 +0300

    Upload

--- JS Essentials - 2019.05.14/04. String And RegEx/LAB/02_Find-Ascii-Equivalent/solution.js
+++ JS Essentials - 2019.05.14/04. String And RegEx/LAB/02_Find-Ascii-Equivalent/solution.js
@@ -1,2 +1,32 @@
 function solve() {
+  let input = document.getElementById('text').value;
+  let result = document.getElementById('result');
+
+  function ascii(input) {
+    let inputElemets = input.split(' ').filter(a => a !== '');
+    let output = '';
+
+    for (const element of inputElemets) {
+      if (Number(element)) {
+        output += (String.fromCharCode(element));
+      } else {
+        let numbers = [];
+
+        for (let i = 0; i < element.length; i++) {
+          let charToNum = element[i].charCodeAt(0);
+          numbers.push(charToNum);
+        }
+
+        let p = document.createElement('p');
+        p.innerHTML = numbers.join(' ');
+        result.appendChild(p);
+      }
+    }
+
+    let p = document.createElement('p');
+    p.innerHTML = output;
+    result.appendChild(p);
+  }
+
+  ascii(input);
 }
