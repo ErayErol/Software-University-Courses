2cc60bee7bf0a403c62f1811f9df74adb2a047c3
ErayErol <erayerolbmw@gmail.com>
2019-08-20 12:59:08 +0300

    Upload

--- JS Essentials - 2019.05.14/01. JavaScript Syntax, Functions and Statements/LAB/04. Largest Number.js
+++
@@ -1,5 +0,0 @@
-function solve(num1, num2, num3) {
-
-    let result = Math.max(Math.max(num1, num2), num3);
-    console.log(`The largest number is ${result}.`);
-}
--- JS Essentials - 2019.05.14/03. Arrays and Matrices/Exercises/08. Spiral Matrix.js
+++
@@ -1,7 +0,0 @@
-function solve(matrix) {
-    
-}
-
-solve(5, 5);
-
-solve(3, 3);
