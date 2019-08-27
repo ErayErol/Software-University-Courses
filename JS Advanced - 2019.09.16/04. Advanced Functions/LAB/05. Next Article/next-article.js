85b30e824c38a45ff62c70641d5c2928a067fc14
ErayErol <erayerolbmw@gmail.com>
2019-08-27 09:33:20 +0300

    Upload

--- JS Advanced - 2019.09.16/04. Advanced Functions/LAB/05. Next Article/next-article.js
+++ JS Advanced - 2019.09.16/04. Advanced Functions/LAB/05. Next Article/next-article.js
@@ -1,3 +1,15 @@
-function getArticleGenerator(input){   
-	//TODO...
+function getArticleGenerator(articles) {
+	let content = document.getElementById('content');
+
+	return function () {
+		if (articles.length > 0) {
+			let p = document.createElement('p');
+			p.textContent = articles.shift();
+
+			let article = document.createElement('article');
+			article.appendChild(p);
+
+			content.appendChild(article);
+		}
+	};
 }
