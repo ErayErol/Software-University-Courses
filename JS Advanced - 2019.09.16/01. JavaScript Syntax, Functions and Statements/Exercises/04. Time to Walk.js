5081fe2a2347b7eb5d77600b58c1f54a5f7be56b
ErayErol <erayerolbmw@gmail.com>
2019-08-31 00:52:12 +0300

    Upload

--- JS Advanced - 2019.09.16/01. JavaScript Syntax, Functions and Statements/Exercises/04. Time to Walk.js
+++ JS Advanced - 2019.09.16/01. JavaScript Syntax, Functions and Statements/Exercises/04. Time to Walk.js
@@ -1,3 +1,27 @@
-function solve(input) {
-    
+function solve(step, lengthOfStep, speed) {
+    let distance = step * lengthOfStep / 1000;
+    let speedForSecond = speed / 3600;
+
+    let seconds = Math.round(distance / speedForSecond);
+    let minutes = Math.floor(step * lengthOfStep / 500);
+    let hours = 0;
+
+    while (seconds > 60) {
+        seconds -= 60;
+        minutes++;
+    }
+
+    while (minutes > 60) {
+        minutes -= 60;
+        hours++;
+    }
+
+    seconds = seconds > 10 ? seconds : `0${seconds}`;
+    minutes = minutes > 10 ? minutes : `0${minutes}`;
+    hours = hours > 10 ? hours : `0${hours}`;
+    console.log(`${hours}:${minutes}:${seconds}`);
 }
+
+solve(4000, 0.60, 5);
+
+solve(2564, 0.70, 5.5);
