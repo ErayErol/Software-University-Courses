da6fb7ce5a24c47bd6637fb9b7ebb34fd21000cf
ErayErol <erayerolbmw@gmail.com>
2019-08-30 11:49:48 +0300

    Upload

---
+++ JS Advanced - 2019.09.16/01. JavaScript Syntax, Functions and Statements/LAB/04. Largest Number.js
@@ -0,0 +1,9 @@
+function solve(num1, num2, num3) {
+    let arrNums = [num1, num2, num3];
+
+    console.log(`The largest number is ${Math.max(...arrNums)}.`);
+}
+
+solve(5, -3, 16);
+
+solve(-3, -5, -22.5);
---
+++ JS Advanced - 2019.09.16/01. JavaScript Syntax, Functions and Statements/LAB/05. Circle Area.js
@@ -0,0 +1,14 @@
+function solve(input) {
+    let inputType = typeof (input);
+
+    if (inputType === 'number') {
+        let result = Math.pow(input, 2) * Math.PI;
+        console.log(result.toFixed(2));
+    } else {
+        console.log(`We can not calculate the circle area, because we receive a ${inputType}.`);
+    }
+}
+
+solve(5);
+
+solve('name');
