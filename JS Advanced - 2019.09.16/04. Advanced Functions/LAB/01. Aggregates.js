1081b1c91e0f0e34e1a739d9fea1cd7866c61008
ErayErol <erayerolbmw@gmail.com>
2019-09-13 09:47:35 +0300

    Upload

--- JS Advanced - 2019.09.16/04. Advanced Functions/LAB/01. Aggregates.js
+++ JS Advanced - 2019.09.16/04. Advanced Functions/LAB/01. Aggregates.js
@@ -4,7 +4,7 @@ const solution = function (arr) {
     console.log('Max = ' + arr.reduce((a, b) => Math.max(a, b)));
     console.log('Product = ' + arr.reduce((a, b) => a * b));
     console.log('Join = ' + arr.reduce((a, b) => `${a}${b}`));
-}
+};
 
 solution([2, 3, 10, 5]);
 
