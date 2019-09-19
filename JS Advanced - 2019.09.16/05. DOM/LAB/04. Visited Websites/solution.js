e738396aa51da8124c9fd54a1a4ba7970b53d2b4
ErayErol <erayerolbmw@gmail.com>
2019-09-20 01:03:44 +0300

    Upload

--- JS Advanced - 2019.09.16/05. DOM/LAB/04. Visited Websites/solution.js
+++ JS Advanced - 2019.09.16/05. DOM/LAB/04. Visited Websites/solution.js
@@ -1,4 +1,20 @@
 function solve() {
+  
+  function f(c) {
+    document.querySelectorAll('a')[c].addEventListener("click", function(){
+      let x = document.querySelectorAll('p')[c].innerHTML.split(' ');
+      let y = document.querySelectorAll('p')[c];
+      
+      x[1]++;
+      y.innerHTML = '';
+      y.innerHTML = x.join(' ');
+    });
+  }
 
-  //TODO...
+  f(0);
+  f(1);
+  f(2);
+  f(3);
+  f(4);
+  f(5);
 }
