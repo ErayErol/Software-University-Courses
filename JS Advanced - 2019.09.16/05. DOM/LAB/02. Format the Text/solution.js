fd6580293ed8dd06b3c088034455e07d0f30ad0c
ErayErol <erayerolbmw@gmail.com>
2019-09-19 18:22:42 +0300

    Upload

--- JS Advanced - 2019.09.16/05. DOM/LAB/02. Format the Text/solution.js
+++ JS Advanced - 2019.09.16/05. DOM/LAB/02. Format the Text/solution.js
@@ -1,3 +1,34 @@
 function solve() {
-  //TODO...
+  let inputText = document.getElementById('input').textContent;
+  let words = inputText.match(/\S+/mg);
+  let mergeWords = '';
+  let sentences = [];
+
+  while (words.length > 0) {
+    let word = words.shift();
+    mergeWords += `${word} `;
+
+    if (word.endsWith('.')) {
+      sentences.push(mergeWords);
+      mergeWords = '';
+    }
+  }
+
+  let output = document.getElementById('output');
+  let restLength = sentences.length % 3;
+  
+  while (sentences.length > 3) {
+    let p = document.createElement('p');
+    for (let i = 0; i < 3; i++) {
+      let sentence = sentences.shift();
+      p.textContent += sentence;
+    }
+    output.appendChild(p);
+  }
+
+  let p = document.createElement('p');
+  for (let i = 0; i < restLength; i++) {
+    p.textContent += sentences.shift();
+  }
+  output.appendChild(p);
 }
