1ce484bc12764d02fbf964161bbe8413b3bd962b
ErayErol <erayerolbmw@gmail.com>
2019-09-24 23:37:25 +0300

    Upload

--- JS Advanced - 2019.09.16/05. DOM/Exercises/07. Shopping-Cart/solution.js
+++ JS Advanced - 2019.09.16/05. DOM/Exercises/07. Shopping-Cart/solution.js
@@ -1,5 +1,31 @@
 function solve() {
-   document.querySelectorAll('button').addEventListener('click', (e) => {
-      
+   let totalSum = 0;
+   let products = [];
+   let buttons = document.getElementsByTagName('button');
+   let textArea = document.getElementsByTagName('textarea')[0];
+   let disable = true;
+
+   Array.from(buttons).forEach(add => {
+      add.addEventListener('click', function (e) {
+         if (disable === true) {
+            let t = e.target;
+            
+            if (t.className === 'add-product') {
+               let product = t.parentNode.parentNode;
+               let name = product.children[1].children[0].textContent;
+               let money = Number(product.children[3].textContent);
+   
+               textArea.textContent += `Added ${name} for ${money.toFixed(2)} to the cart.\n`;
+   
+               totalSum += money;
+               products.push(name);
+            } else {
+               disable = false;
+               let set = new Set(products);
+               products = Array.from(set);
+               textArea.textContent += `You bought ${products.join(', ')} for ${totalSum.toFixed(2)}.`;
+            }
+         }
+      });
    });
 }
