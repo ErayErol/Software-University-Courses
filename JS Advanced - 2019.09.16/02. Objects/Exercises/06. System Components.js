f5f3c4b33d869d66e3f53d65e00395e64ca5e22d
ErayErol <erayerolbmw@gmail.com>
2019-09-25 22:07:45 +0300

    Upload

--- JS Advanced - 2019.09.16/02. Objects/Exercises/06. System Components.js
+++ JS Advanced - 2019.09.16/02. Objects/Exercises/06. System Components.js
@@ -42,17 +42,20 @@ function systemComponents(input) {
     }
 }
 
-systemComponents(['SULS | Main Site | Home Page',
-    'SULS | Main Site | Login Page',
-    'SULS | Main Site | Register Page',
-    'SULS | Judge Site | Login Page',
-    'SULS | Judge Site | Submittion Page',
-    'Lambda | CoreA | A23',
-    'SULS | Digital Site | Login Page',
-    'Lambda | CoreB | B24',
-    'Lambda | CoreA | A24',
-    'Lambda | CoreA | A25',
-    'Lambda | CoreC | C4',
-    'Indice | Session | Default Storage',
-    'Indice | Session | Default Security']
+systemComponents(
+    [
+        'SULS | Main Site | Home Page',
+        'SULS | Main Site | Login Page',
+        'SULS | Main Site | Register Page',
+        'SULS | Judge Site | Login Page',
+        'SULS | Judge Site | Submittion Page',
+        'Lambda | CoreA | A23',
+        'SULS | Digital Site | Login Page',
+        'Lambda | CoreB | B24',
+        'Lambda | CoreA | A24',
+        'Lambda | CoreA | A25',
+        'Lambda | CoreC | C4',
+        'Indice | Session | Default Storage',
+        'Indice | Session | Default Security'
+    ]
 );
--- JS Advanced - 2019.09.16/05. DOM/Exercises/08. Furniture/solution.js
+++ JS Advanced - 2019.09.16/05. DOM/Exercises/08. Furniture/solution.js
@@ -1,4 +1,47 @@
 function solve() {
+  let tbody = document.getElementsByTagName('tbody')[0];
+  let rows = document.querySelectorAll('tbody tr')[0];
+  rows.cells[4].children[0].disabled = false;
+  
+  let textAreas = document.getElementsByTagName('textarea');
+  let btn = document.getElementsByTagName('button');
+  
+  let generate = btn[0];
+  let generateArea = textAreas[0];
+  
+  generate.addEventListener('click', () => {
+    let furniture = JSON.parse(generateArea.value);
 
-  //TODO...
+    let row = rows.cloneNode(true);
+    row.cells[0].children[0].src = furniture[0].img;
+    row.cells[1].children[0].textContent = furniture[0].name;
+    row.cells[2].children[0].textContent = furniture[0].price;
+    row.cells[3].children[0].textContent = furniture[0].decFactor;
+    row.cells[4].children[0].disabled = false;
+
+    tbody.appendChild(row);
+  });
+
+
+  let buy = btn[1];
+  let buyArea = textAreas[1];
+
+  buy.addEventListener('click', () => {
+    let avrg = 0;
+    let totalPrice = 0;
+    let boughtFurnitures = [];
+
+    for (let row of tbody.children) {
+
+      if (row.cells[4].children[0].checked == true) {
+        totalPrice += Number(row.cells[2].children[0].textContent);
+        boughtFurnitures.push(row.cells[1].children[0].textContent);
+        avrg += Number(row.cells[3].children[0].textContent);
+      }
+    }
+
+    buyArea.textContent += `Bought furniture: ${boughtFurnitures.join(', ')}\n`;
+    buyArea.textContent += `Total price: ${totalPrice.toFixed(2)}\n`;
+    buyArea.textContent += `Average decoration factor: ${avrg / boughtFurnitures.length}`;
+  });
 }
