2065ab804d4bd6c05144c3079ef738e385f4a5aa
ErayErol <erayerolbmw@gmail.com>
2019-09-29 00:21:51 +0300

    Upload

--- JS Advanced - 2019.09.16/06. DOM Manipulations/LAB/03. Delete-From-Table/app.js
+++ JS Advanced - 2019.09.16/06. DOM Manipulations/LAB/03. Delete-From-Table/app.js
@@ -1,3 +1,18 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    input = document.getElementsByTagName('input')[0];
+    result = document.getElementById('result');
+
+    let hasDelete = false;
+    let secondColumn = document.querySelectorAll("#customers tr td:nth-child(2)");
+    for (let td of secondColumn) {
+        if (td.textContent.includes(input.value)) {
+            let parent = td.parentNode;
+            hasDelete = true;
+            parent.remove();
+        }
+    }
+
+    hasDelete === true
+        ? result.textContent = 'Deleted.'
+        : result.textContent = 'Not found.';
 }
