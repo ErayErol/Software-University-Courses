8adf4d183f40c04225965474d93fac35911e9bfe
ErayErol <erayerolbmw@gmail.com>
2019-09-29 09:11:12 +0300

    Upload

--- JS Advanced - 2019.09.16/06. DOM Manipulations/LAB/03. Delete-From-Table/app.js
+++ JS Advanced - 2019.09.16/06. DOM Manipulations/LAB/03. Delete-From-Table/app.js
@@ -1,14 +1,16 @@
 function deleteByEmail() {
     input = document.getElementsByTagName('input')[0];
     result = document.getElementById('result');
-
+    
     let hasDelete = false;
-    let secondColumn = document.querySelectorAll("#customers tr td:nth-child(2)");
-    for (let td of secondColumn) {
-        if (td.textContent.includes(input.value)) {
-            let parent = td.parentNode;
+    let emails = document.querySelectorAll("#customers tr td:nth-child(2)");
+    
+    for (let cell of emails) {
+        let email = cell.textContent;
+
+        if (email === input.value) {
+            cell.parentNode.remove();
             hasDelete = true;
-            parent.remove();
         }
     }
 
