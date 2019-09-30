45eedab23ab86a1e7ea4097158f26841acadc9c2
ErayErol <erayerolbmw@gmail.com>
2019-09-30 12:12:24 +0300

    Upload

--- JS Advanced - 2019.09.16/06. DOM Manipulations/LAB/07. Dynamic-Validation/app.js
+++ JS Advanced - 2019.09.16/06. DOM Manipulations/LAB/07. Dynamic-Validation/app.js
@@ -1,3 +1,12 @@
 function validate() {
-    console.log('TODO:...');
+    let email = document.getElementById('email');
+    let regex = /[a-z]+@[a-z]+.[a-z]+/;
+
+    email.addEventListener('change', (event) => {
+        if (email.value.match(regex)) {
+            event.target.className = '';
+        }else {
+            event.target.className = 'error';
+        }
+    });
 }
