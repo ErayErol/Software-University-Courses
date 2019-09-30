ecc9e4f9e71aae75b0c418e7f8ab7792afa29e97
ErayErol <erayerolbmw@gmail.com>
2019-09-30 23:12:43 +0300

    Upload

--- JS Advanced - 2019.09.16/06. DOM Manipulations/Exercises/02. Fill-Dropdown/dropdown.js
+++ JS Advanced - 2019.09.16/06. DOM Manipulations/Exercises/02. Fill-Dropdown/dropdown.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+    let newItemText = document.getElementById('newItemText');
+    let newItemValue = document.getElementById('newItemValue');
+
+    let option = document.createElement('option');
+    option.textContent = newItemText.value;
+    option.value = newItemValue.value;
+    
+    let select = document.getElementById('menu');
+    select.add(option);
+
+    newItemText.value = '';
+    newItemValue.value = '';
 }
