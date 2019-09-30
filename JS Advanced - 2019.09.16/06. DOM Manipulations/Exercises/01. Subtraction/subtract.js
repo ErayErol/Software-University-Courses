b38d036ea6bdf55a110786810564ff1e388dbd99
ErayErol <erayerolbmw@gmail.com>
2019-09-30 22:13:32 +0300

    Upload

--- JS Advanced - 2019.09.16/06. DOM Manipulations/Exercises/01. Subtraction/subtract.js
+++ JS Advanced - 2019.09.16/06. DOM Manipulations/Exercises/01. Subtraction/subtract.js
@@ -1,3 +1,6 @@
 function subtract() {
-    console.log('TODO:...');
+    let x = Number(document.getElementById('firstNumber').value);
+    let y = Number(document.getElementById('secondNumber').value);
+    let result = document.getElementById('result');
+    result.textContent = x - y;
 }
