8fa00959c1199e02ef411292d2dc64614dedee57
ErayErol <erayerolbmw@gmail.com>
2019-10-01 12:12:48 +0300

    Upload

--- JS Advanced - 2019.09.16/06. DOM Manipulations/Exercises/04. Sections/script.js
+++ JS Advanced - 2019.09.16/06. DOM Manipulations/Exercises/04. Sections/script.js
@@ -1,6 +1,6 @@
 function create(words) {
    let content = document.getElementById('content');
-   
+
    for (const word of words) {
       let div = document.createElement('div');
       let p = document.createElement('p');
@@ -8,7 +8,7 @@ function create(words) {
       p.textContent = word;
 
       div.appendChild(p);
-      div.addEventListener('click', show)
+      div.addEventListener('click', show);
       content.appendChild(div);
    }
    
