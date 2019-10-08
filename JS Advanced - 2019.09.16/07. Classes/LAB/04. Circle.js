dd46db34377173d2d965f202b863370f2cdff540
ErayErol <erayerolbmw@gmail.com>
2019-10-08 21:29:28 +0300

    Upload

--- JS Advanced - 2019.09.16/07. Classes/LAB/04. Circle.js
+++ JS Advanced - 2019.09.16/07. Classes/LAB/04. Circle.js
@@ -1,5 +1,5 @@
 class Circle {
-    constructor(radius) {
+    constructor(radius){
         this.radius = radius;
     }
 
@@ -10,8 +10,20 @@ class Circle {
     set diameter(diameter) {
         this.radius = diameter / 2;
     }
-    
+
     get area() {
         return Math.PI * this.radius * this.radius;
     }
 }
+
+let c = new Circle(2);
+
+console.log(`Radius: ${c.radius}`);
+console.log(`Diameter: ${c.diameter}`);
+console.log(`Area: ${c.area}`);
+
+c.diameter = 1.6;
+
+console.log(`Radius: ${c.radius}`);
+console.log(`Diameter: ${c.diameter}`);
+console.log(`Area: ${c.area}`);
