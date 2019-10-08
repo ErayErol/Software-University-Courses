cc459dd38aaf59eba4caa787e4b509fd8f28529e
ErayErol <erayerolbmw@gmail.com>
2019-10-08 13:48:55 +0300

    Upload

--- JS Advanced - 2019.09.16/07. Classes/LAB/01. Rectangle.js
+++ JS Advanced - 2019.09.16/07. Classes/LAB/01. Rectangle.js
@@ -10,7 +10,6 @@ class Rectangle {
     }
 }
 
-
 let rect = new Rectangle(4, 5, 'red');
 console.log(rect.width);
 console.log(rect.height);
---
+++ JS Advanced - 2019.09.16/07. Classes/LAB/04. Circle.js
@@ -0,0 +1,17 @@
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    get diameter() {
+        return 2 * this.radius;
+    }
+
+    set diameter(diameter) {
+        this.radius = diameter / 2;
+    }
+    
+    get area() {
+        return Math.PI * this.radius * this.radius;
+    }
+}
