6abbb9d142c8bbde9f4243d6c299560417d3a3b2
ErayErol <erayerolbmw@gmail.com>
2019-10-08 23:40:51 +0300

    Upload

---
+++ JS Advanced - 2019.09.16/07. Classes/LAB/06. Cards.js
@@ -0,0 +1,58 @@
+const result = (function () {
+    let card = {
+        faces: ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"],
+        suits: {
+            CLUBS: "\u2663",
+            DIAMONDS: "\u2666",
+            HEARTS: "\u2665",
+            SPADES: "\u2660"
+        },
+
+    };
+
+    class Card {
+        constructor(face, suit) {
+            this.face = face;
+            this.suit = suit;
+        }
+
+        get face() {
+            return this._face;
+        }
+
+        set face(face) {
+            if (!card.faces.includes(face)) {
+                throw new Error("Invalid face");
+            }
+            this._face = face;
+        }
+
+        get suit() {
+            return this._suit;
+        }
+
+        set suit(suit) {
+            if (!Object.values(card.suits).some(x => x === suit)) {
+                throw new Error("Invalid suit");
+            }
+            this._suit = suit;
+        }
+    }
+
+    return {
+        Suits: card.suits,
+        Card: Card
+    };
+}());
+
+let Card = result.Card;
+let Suits = result.Suits;
+
+try {
+    let card = new Card("Q", Suits.CLUBS);
+    card.face = "A";
+    card.suit = Suits.DIAMONDS;
+    let card2 = new Card("1", Suits.DIAMONDS);
+} catch (error) {
+    console.log(error);
+}
