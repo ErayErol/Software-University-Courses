4cbf7cc0b059b17c1153cff82962ce64c940b731
ErayErol <erayerolbmw@gmail.com>
2019-12-08 19:51:22 +0200

    Upload

--- JS Advanced - 2019.09.16/JS Advanced - Exams/Old/2019.07.07/01/scripts/index.js
+++ JS Advanced - 2019.09.16/JS Advanced - Exams/Old/2019.07.07/01/scripts/index.js
@@ -1,127 +1,117 @@
 function mySolution() {
-    let textarea = document.querySelector('#inputSection textarea');
-    let input = document.querySelector('#inputSection input');
-    let questionBtn = document.querySelector('#inputSection button');
-    let pendingQuestions = document.getElementById('pendingQuestions');
-    let openQuestions = document.getElementById('openQuestions');
+    let username = document.getElementsByTagName('input')[0];
+    let textarea = document.getElementsByTagName('textarea')[0];
+    let [_, pendingQuestion, openQuestion] = Array.from(document.getElementsByTagName('div'));
 
-    questionBtn.addEventListener('click', (e) => {
-            let textareaCopy = textarea.value;
-            textarea.value = '';
-            let name = input.value;
-            input.value = '';
+    let btnSend1 = document.getElementsByTagName('button')[0];
+    btnSend1.addEventListener('click', sendFunc);
+    function sendFunc(e) {
+        if (textarea.value) {
+            let divPQ1 = document.createElement('div');
+            divPQ1.className = 'pendingQuestion';
 
-            let div1 = document.createElement('div');
-            div1.className = 'pendingQuestion';
-            let img1 = document.createElement('img');
-            img1.setAttribute('src', './images/user.png');
-            img1.setAttribute('width', 32);
-            img1.setAttribute('height', 32);
-            let span1 = document.createElement('span');
-            (name !== '')
-                ? span1.textContent = name
-                : span1.textContent = 'Anonymous';
-            let p1 = document.createElement('p');
-            p1.textContent = textareaCopy;
-            let div2 = document.createElement('div');
-            div2.className = 'actions';
-            let btn1 = document.createElement('button');
-            btn1.className = 'archive';
-            btn1.textContent = 'Archive';
-            let btn2 = document.createElement('button');
-            btn2.className = 'open';
-            btn2.textContent = 'Open';
-            div2.appendChild(btn1);
-            div2.appendChild(btn2);
-            div1.appendChild(img1);
-            div1.appendChild(span1);
-            div1.appendChild(p1);
-            div1.appendChild(div2);
-            pendingQuestions.appendChild(div1);
+            let img = document.createElement('img');
+            img.src = './images/user.png';
+            img.width = 32;
+            img.height = 32;
+            divPQ1.appendChild(img);
 
-            btn1.addEventListener('click', (e) => {
-                pendingQuestions.removeChild(div1);
-            });
+            let span = document.createElement('span');
+            if (!username.value) {
+                span.textContent = 'Anonymous';
+            } else {
+                span.textContent = username.value;
+            }
+            divPQ1.appendChild(span);
 
-            btn2.addEventListener('click', (e) => {
-                pendingQuestions.removeChild(div1);
+            let p = document.createElement('p');
+            p.textContent = textarea.value;
+            divPQ1.appendChild(p);
 
+            let divPQBtns = document.createElement('div');
+            divPQBtns.className = 'actions';
 
+            let btnArchive = document.createElement('button');
+            btnArchive.textContent = 'Archive';
+            btnArchive.className = 'archive';
+            divPQBtns.appendChild(btnArchive);
 
-                let div3 = document.createElement('div');
-                div3.className = 'openQuestion';
+            let btnOpen = document.createElement('button');
+            btnOpen.textContent = 'Open';
+            btnOpen.className = 'open';
+            divPQBtns.appendChild(btnOpen);
 
-                let img2 = document.createElement('img');
-                img2.setAttribute('src', './images/user.png');
-                img2.setAttribute('width', 32);
-                img2.setAttribute('height', 32);
+            divPQ1.appendChild(divPQBtns);
+            pendingQuestion.appendChild(divPQ1);
 
-                let span2 = document.createElement('span');
-                (name !== '')
-                    ? span2.textContent = name
-                    : span2.textContent = 'Anonymous';
+            btnArchive.addEventListener('click', archiveFunc);
+            function archiveFunc(e) {
+                e.target.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode);
+            }
 
-                let p2 = document.createElement('p');
-                p2.textContent = textareaCopy;
+            btnOpen.addEventListener('click', openFunc);
+            function openFunc(e) {
+                let divOQ1 = document.createElement('div');
+                divOQ1.className = 'openQuestion';
+                divOQ1.appendChild(e.target.parentNode.parentNode.children[0]);
+                divOQ1.appendChild(e.target.parentNode.parentNode.children[0]);
+                divOQ1.appendChild(e.target.parentNode.parentNode.children[0]);
 
-                let div4 = document.createElement('div');
-                div4.className = 'actions';
+                e.target.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode);
 
-                let btn3 = document.createElement('button');
-                btn3.className = 'reply';
-                btn3.textContent = 'Reply';
+                let divOQBtn = document.createElement('div');
+                divOQBtn.className = 'actions';
 
+                let btnReply = document.createElement('button');
+                btnReply.textContent = 'Reply';
+                btnReply.className = 'reply';
+                divOQBtn.appendChild(btnReply);
 
-                let div5 = document.createElement('div');
-                div5.className = 'replySection';
-                div5.style.display = 'none';
+                divOQ1.appendChild(divOQBtn);
 
-                let input2 = document.createElement('input');
-                input2.className = 'replyInput';
-                input2.type = 'text';
-                input2.placeholder = 'Reply to this question here...';
+                let divReply = document.createElement('div');
+                divReply.className = 'replySection';
 
-                let btn4 = document.createElement('button');
-                btn4.className = 'replyButton';
-                btn4.textContent = 'Send';
+                let input = document.createElement('input');
+                input.className = 'replyInput';
+                input.type = 'text';
+                input.placeholder = 'Reply to this question here...';
+                divReply.appendChild(input);
+
+                let btnSend2 = document.createElement('button');
+                btnSend2.className = 'replyButton';
+                btnSend2.textContent = 'Send';
+                divReply.appendChild(btnSend2);
 
                 let ol = document.createElement('ol');
                 ol.className = 'reply';
                 ol.type = '1';
+                divReply.appendChild(ol);
+                
+                divReply.style.display = 'none';
+                divOQ1.appendChild(divReply);
+                openQuestion.appendChild(divOQ1);
 
-                div4.appendChild(btn3);
+                btnReply.addEventListener('click', replyFunc);
+                function replyFunc(e) {
+                    if (btnReply.textContent === 'Reply') {
+                        divReply.style.display = 'block';
+                        btnReply.textContent = 'Back';
 
-                div5.appendChild(input2);
-                div5.appendChild(btn4);
-                div5.appendChild(ol);
-
-                div3.appendChild(img2);
-                div3.appendChild(span2);
-                div3.appendChild(p2);
-                div3.appendChild(div4);
-                div3.appendChild(div5);
-                openQuestions.appendChild(div3);
-
-                btn3.addEventListener('click', (е) => {
-
-                    if (btn3.textContent === 'Reply') {
-                        div5.style.display = 'block';
-                        btn3.textContent = 'Back';
-
-                        btn4.addEventListener('click', (er) => {
-                            if (input2.value) {
+                        btnSend2.addEventListener('click', sendFunc2);
+                        function sendFunc2(e) {
+                            if (input.value) {
                                 let li = document.createElement('li');
-                                li.textContent = input2.value;
+                                li.textContent = input.value;
                                 ol.appendChild(li);
-
-                                input2.value = '';
                             }
-                        });
-                    } else {
-                        div5.style.display = 'none';
-                        btn3.textContent = 'Reply';
+                        }
+                    } else if (btnReply.textContent === 'Back') {
+                        divReply.style.display = 'none';
+                        btnReply.textContent = 'Reply';
                     }
-                });
-            });
-    });
+                }
+            }
+        }
+    }
 }
