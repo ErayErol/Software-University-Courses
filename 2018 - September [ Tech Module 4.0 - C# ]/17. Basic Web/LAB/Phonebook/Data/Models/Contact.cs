d35f2633c7dc2ad4739896ace32efc6b8dffc1f9
ErayErol <erayerolbmw@gmail.com>
2020-12-13 15:38:50 +0200

    Upload

---
+++ 2018 - September [ Tech Module 4.0 - C# ]/17. Basic Web/LAB/Phonebook/Data/Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Phonebook.Data.Models
+{
+    public class Contact
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Number { get; set; }
+    }
+}
