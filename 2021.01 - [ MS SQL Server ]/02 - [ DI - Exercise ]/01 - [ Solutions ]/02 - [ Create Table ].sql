8e1653eb08ffabe8628d95880e08dd2bca7548fd
ErayErol <erayerolbmw@gmail.com>
2021-01-13 05:47:18 +0200

    Upload

---
+++ 2021.01 - [ MS SQL Server ]/02 - [ DI - Exercise ]/01 - [ Solutions ]/02 - [ Create Table ].sql
@@ -0,0 +1,12 @@
+CREATE TABLE Minions 
+(
+	Id INT PRIMARY KEY,
+	[Name] NVARCHAR(50),
+	Age TINYINT
+)
+
+CREATE TABLE Towns 
+(
+	Id INT PRIMARY KEY,
+	[Name] NVARCHAR(50),
+)
---
+++ 2021.01 - [ MS SQL Server ]/02 - [ DI - Exercise ]/01 - [ Solutions ]/03 - [ Alter Minions Table ].sql
@@ -0,0 +1,5 @@
+ALTER TABLE Minions ADD TownId INT
+
+ALTER TABLE Minions 
+	ADD CONSTRAINT FK_Minion_Town 
+		FOREIGN KEY (TownId) REFERENCES Towns (Id);
