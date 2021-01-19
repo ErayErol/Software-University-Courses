5a8794599306ef6ad5a2002b3ba6fec6c25e3422
ErayErol <erayerolbmw@gmail.com>
2021-01-19 17:14:09 +0200

    Upload

--- 2021.01 - [ MS SQL Server ]/06 - [ Table Relations - Exercise ]/01 - [ Solutions ]/03 - [ Many-To-Many Relationship ].sql
+++ 2021.01 - [ MS SQL Server ]/06 - [ Table Relations - Exercise ]/01 - [ Solutions ]/03 - [ Many-To-Many Relationship ].sql
@@ -1,2 +1,51 @@
-SELECT [Name] 
-FROM Departments
+CREATE TABLE Students
+(
+	StudentID	INT IDENTITY PRIMARY KEY,
+	[Name]		NVARCHAR(50) NOT NULL,
+)
+
+CREATE TABLE Exams
+(
+	ExamID	INT IDENTITY(101, 1) PRIMARY KEY,
+	[Name]	NVARCHAR(50) NOT NULL,
+)
+
+CREATE TABLE StudentsExams
+(
+	StudentID	INT,
+	ExamID		INT,
+		CONSTRAINT PK_StudentsStudentID_ExamsExamID
+		PRIMARY KEY(StudentID, ExamID),
+		
+		CONSTRAINT FK_StudentsExams_Students
+		FOREIGN KEY(StudentID)
+		REFERENCES Students(StudentID),
+
+		CONSTRAINT FK_StudentsExams_Exams
+		FOREIGN KEY(ExamID)
+		REFERENCES Exams(ExamID)
+)
+
+INSERT INTO Students
+([Name])
+VALUES
+('Mila'),
+('Toni'),
+('Ron')
+
+INSERT INTO Exams
+([Name])
+VALUES
+('SpringMVC'),
+('Oracle '),
+('Oracle 11g')
+
+INSERT INTO StudentsExams
+(StudentID, ExamID)
+VALUES
+(1, 101),
+(1, 102),
+(2, 101),
+(3, 103),
+(2, 102),
+(2, 103)
