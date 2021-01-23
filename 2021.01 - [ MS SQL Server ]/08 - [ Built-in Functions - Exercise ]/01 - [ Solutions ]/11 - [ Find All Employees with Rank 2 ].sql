03878da1b817a9cc9b8eeba62f64f8e728b1579f
ErayErol <erayerolbmw@gmail.com>
2021-01-23 06:40:40 +0200

    Upload

--- 2021.01 - [ MS SQL Server ]/08 - [ Built-in Functions - Exercise ]/01 - [ Solutions ]/11 - [ Find All Employees with Rank 2 ].sql
+++ 2021.01 - [ MS SQL Server ]/08 - [ Built-in Functions - Exercise ]/01 - [ Solutions ]/11 - [ Find All Employees with Rank 2 ].sql
@@ -1,3 +1,13 @@
-SELECT FirstName, LastName
-FROM Employees
-WHERE ManagerID IS NULL
+SELECT * FROM
+(
+	SELECT 
+		EmployeeID,
+		FirstName, 
+		LastName, 
+		Salary,
+		DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID) AS Rank
+		FROM Employees
+		WHERE (Salary BETWEEN 10000 AND 50000)
+) AS OrderEmployees
+	WHERE OrderEmployees.Rank = 2
+	ORDER BY OrderEmployees.Salary DESC
