3b9be6ddd836e93861e1d242c2044772d28eaab9
ErayErol <erayerolbmw@gmail.com>
2021-01-26 09:05:00 +0200

    Upload

--- 2021.01 - [ MS SQL Server ]/09 - [ Subqueries and Joins - LAB ]/01 - [ Solutions ]/04 - [ Employee Summary ].sql
+++ 2021.01 - [ MS SQL Server ]/09 - [ Subqueries and Joins - LAB ]/01 - [ Solutions ]/04 - [ Employee Summary ].sql
@@ -1,9 +1,9 @@
-SELECT
+SELECT TOP(50)
 	e.EmployeeID,
 	CONCAT(e.FirstName, ' ', e.LastName) AS EmployeeName,
-	CONCAT(e2.FirstName, ' ', e2.LastName) AS ManagerName,
+	CONCAT(m.FirstName, ' ', m.LastName) AS ManagerName,
 	d.[Name] AS DepartmentName
   FROM Employees AS e
-	JOIN Departments AS d ON d.DepartmentID = e.DepartmentID
-	JOIN Employees AS e2 ON e2.EmployeeID = e.ManagerID
+	LEFT JOIN Employees AS m ON m.EmployeeID = e.ManagerID
+	LEFT JOIN Departments AS d ON d.DepartmentID = e.DepartmentID
   ORDER BY e.EmployeeID
