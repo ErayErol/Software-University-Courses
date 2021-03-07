71c9bcf0f638280652995a9009e22b3064064d06
ErayErol <erayerolbmw@gmail.com>
2021-03-07 18:23:53 +0200

    Upload

---
+++ 2021.02 - [ Entity Framework Core ]/15 - [ Auto Mapping Objects  - Exercise ]/01 - [ Solutions ]/00 - [ BookShop Database ]/BookShop/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+        }
+    }
+}
---
+++ 2021.02 - [ Entity Framework Core ]/15 - [ Auto Mapping Objects  - Exercise ]/01 - [ Solutions ]/15 - [ Remove Books ]/BookShop/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using BookShop.Models;
+using BookShop.Models.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShop
+{
+    using Data;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            var result = RemoveBooks(db);
+            Console.WriteLine(result);
+        }
+
+        public static int RemoveBooks(BookShopContext context)
+        {
+            var books = context
+                .Books
+                .Where(b => b.Copies < 4200)
+                .ToList();
+
+            context.Books.RemoveRange(books);
+            context.SaveChanges();
+            return books.Count;
+        }
+    }
+}
