11999ce21abb2d1be1d10616fb28104685fbddc8
ErayErol <erayerolbmw@gmail.com>
2021-09-22 17:23:40 +0300

    searching in home page - working successfully

--- 2021.09 - [ JS Back End ]/01 - [ Intro To NodeJS ]/01 - [ Cat Shelter ]/handlers/home.js
+++ 2021.09 - [ JS Back End ]/01 - [ Intro To NodeJS ]/01 - [ Cat Shelter ]/handlers/home.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const fs = require('fs');
 const path = require('path');
+const qs = require('querystring');
 const cats = require('../data/cats');
 
 module.exports = (req, res) => {
@@ -37,6 +38,49 @@ module.exports = (req, res) => {
             res.writeHead(200, { 'Content-Type': 'text/html' });
             res.write(modifiedData);
             res.end();
+        });
+    } else if (pathname === '/search' && req.method === 'POST') {
+        let formData = '';
+
+        req.on('data', (data) => {
+            formData += data;
+        });
+
+        req.on('end', () => {
+            let filePath = path.join(__dirname, '../views/home/index.html');
+
+            fs.readFile(filePath, (err, data) => {
+                if (err) {
+                    console.log(err);
+                    res.writeHead(404, {
+                        'Content-Type': 'text/plain'
+                    });
+                    res.write('404 Not Found.');
+                    res.end();
+                    return;
+                }
+
+                let body = qs.parse(formData);
+                let catFilter = cats.filter(c => c.name.includes(body.search));
+
+                let modifiedCats = catFilter.map(cat => `
+                    <li>
+                        <img src="/content/images/${cat.image}" alt="Cat name : ${cat.name} Cat breed : ${cat.breed}">
+                        <h3>${cat.name}</h3>
+                        <p><span>Breed: </span>${cat.breed}</p>
+                        <p><span>Description: </span>${cat.description}</p>
+                        <ul class="buttons">
+                            <li class="btn edit"><a href="/cats-edit/${cat.id}">Change Info</a></li>
+                            <li class="btn delete"><a href="/cats-find-new-home/${cat.id}">New Home</a></li>
+                        </ul>
+                    </li>
+                `);
+
+                let modifiedData = data.toString().replace('{{cats}}', modifiedCats);
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write(modifiedData);
+                res.end();
+            });
         });
     } else {
         return true;
