b0dcbfed8b063f64335609bd6de3cf8f0b1f1710
ErayErol <erayerolbmw@gmail.com>
2021-10-03 10:02:15 +0300

    Adding presentation

--- 2021.09 - [ JS Back End ]/01 - [ Intro To NodeJS ]/01 - [ Cat Shelter ]/handlers/index.js
+++
@@ -1,5 +0,0 @@
-const homeHandler = require('./home');
-const staticFiles = require('./static-handler');
-const catHandler = require('./cat');
-
-module.exports = [homeHandler, staticFiles, catHandler];
